ed2726da1f7d9c4ded98be4df2b5036bb924a3d4
Rishav054051 <rishavsingh054051@gmail.com>
2024-03-24 16:08:09 +0530

    layout complete

--- calculator.py
+++ calculator.py
@@ -11,22 +11,34 @@ root.geometry("400x600")
 
 
 f1=ct.CTkFrame(root,width=400,height=80,fg_color="black")
-f1.pack(expand=True,fill="both",pady=5)
+f1.pack(fill="both",pady=5)
 f2=ct.CTkFrame(root,width=400,height=800,fg_color="black")
 f2.pack(expand=True,fill="both")
 
 
-# Define buttons for digits (0-9)
-for i in range(10):
-    if i == 0:
-        button = ct.CTkButton(f2, text="0", width=5, height=2, font=('Arial', 14))
-        button.grid(row=4, column=1, padx=5, pady=5)
-    else:
-        button = ct.CTkButton(f2, text=str(i), width=5, height=2, font=('Arial', 14))
-        button.grid(row=(i-1)//3, column=(i-1)%3, padx=5, pady=5)
+# Define buttons for operators and additional functionalities
+buttons = [
+    ("C", 0, 0), ("⌫", 0, 1), ("%", 0, 2), ("/", 0, 3),
+    ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("X", 1, 3),
+    ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("-", 2, 3),
+    ("1", 3, 0), ("2", 3, 1), ("3", 3, 2), ("+", 3, 3),
+    ("e", 4, 0), ("0", 4, 1), (".", 4, 2), ("=", 4, 3)
+]
 
-for i in range(3):
-    f2.grid_rowconfigure(i, weight=1)
+# Create buttons
+for text, row, column in buttons:
+    if (row == 0 and (column >=0 and column < 4)):
+        button = ct.CTkButton(f2, text=text, width=20, height=20,fg_color="black",text_color="orange", font=('Arial', 30))
+        button.grid(row=row, column=column, padx=5, pady=10)
+    elif ((row>=1 and row<5) and (column==3)):
+        button = ct.CTkButton(f2, text=text, width=20, height=20,fg_color="black",text_color="orange", font=('Arial', 30))
+        button.grid(row=row, column=column, padx=5, pady=10)
+    else:
+        button = ct.CTkButton(f2, text=text, width=20, height=20,fg_color="black", font=('Arial', 30))
+        button.grid(row=row, column=column, padx=5, pady=10)
+
+for i in range(4):
+    f2.grid_rowconfigure(i+1, weight=1)
     f2.grid_columnconfigure(i, weight=1)
 
 root.mainloop()
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-for i in range(3):
-    f2.grid_rowconfigure(i, weight=1)
-    f2.grid_columnconfigure(i, weight=1)
