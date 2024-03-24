2fd9f1bb35ea198fb0482dc67989ab2e6a9988fc
Rishav054051 <rishavsingh054051@gmail.com>
2024-03-24 12:54:10 +0530

    appearance

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
-from tkinter import *
 import customtkinter as ct
 
+ct.set_default_color_theme("dark-blue")
+ct.set_appearance_mode("dark")
+
+root=ct.CTk()
+ct.set_widget_scaling(1.5)  # widget dimensions and text size
+ct.set_window_scaling(1.5)  # window geometry dimensions
+
+
+
+root.mainloop()
